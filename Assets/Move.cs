07a8e1d57661c73f8945a9d1eb755ae2f7ba426f
Paul1005 <mccarliep@gmail.com>
2019-01-20 16:27:54 -0800

    finished

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -16,27 +16,40 @@ public class Move : MonoBehaviour
         Rigidbody[] bodies = boat.GetComponentsInChildren<Rigidbody>();
         foreach (Rigidbody body in bodies)
         {
-            Debug.Log(body.mass);
-            Debug.Log(body.position);
+            string name = body.gameObject.name;
+            if (name != "Boat")
+            {
+                Debug.Log("Mass of " + name + ": " + body.mass);
+                Debug.Log("Position of " + name + ": " + body.position);
+            }
 
             mass += body.mass;
             centerOfMass += body.position.z * body.mass;
 
         }
-        Debug.Log(mass);
+        Debug.Log("Mass of " + boat.name + ": " + mass);
         boat.GetComponent<Rigidbody>().mass = mass;
         centerOfMass /= mass;
-        Debug.Log(centerOfMass);
+        Debug.Log("Center of mass for " + boat.name + ": " + centerOfMass);
 
         foreach (Rigidbody body in bodies)
         {
-            Debug.Log(Mathf.Pow(centerOfMass - body.position.z, 2));
-            Debug.Log(Mathf.Pow(centerOfMass - body.position.z, 2) * body.mass);
-            Debug.Log(Mathf.Pow(centerOfMass - body.position.z, 2) * body.mass + body.mass * (Mathf.Pow(body.gameObject.transform.localScale.x, 2) + Mathf.Pow(body.gameObject.transform.localScale.z, 2)) / 12);
-            momentOfInertia += Mathf.Pow(centerOfMass - body.position.z, 2) * body.mass + body.mass * (Mathf.Pow(body.gameObject.transform.localScale.x, 2) + Mathf.Pow(body.gameObject.transform.localScale.z, 2)) / 12;
+            string name = body.gameObject.name;
+            if (name != "Boat")
+            {
+                float h2 = Mathf.Pow(centerOfMass - body.position.z, 2);
+                Debug.Log("h^2 for " + name + ": " + h2);
+                float Mh2 = h2 * body.mass;
+                Debug.Log("Mh^2 for " + name + ": " + Mh2);
+                float ly = body.mass * (Mathf.Pow(body.gameObject.transform.localScale.x, 2) + Mathf.Pow(body.gameObject.transform.localScale.z, 2)) / 12;
+                float inertia = ly + Mh2;
+                Debug.Log("Moment of Inertia for " + name + ": " + inertia);
+
+                momentOfInertia += inertia;
+            }
         }
 
-        Debug.Log(momentOfInertia);
+        Debug.Log("moment of Inertia for " + boat.name + ": " + momentOfInertia);
     }
 
     // Update is called once per frame
