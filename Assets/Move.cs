d6e72e07d9f86749a1714fe7cd34ccc2aeb4a333
Paul1005 <mccarliep@gmail.com>
2019-01-26 00:05:42 -0800

    kinda works

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,21 +13,43 @@ public class Move : MonoBehaviour
     public float centerOfMassX = 0;
     public float momentOfInertia = 0;
 
+    //initial input params
+    public float initialVelocity;
+    public float initialAcceleration;
+    public float totalTime;
+
+    public double finalVelocity;
+    public double stopPosition;
+    public double distance;
+    public double dragConstant;
+    public float deltaTime;
+    public float currentTime;
+    public float startTime;
+    public float actualBoatPosition;
+
+    public int thing;
+
+    private float initialPosition;
     // Use this for initialization
     void Start()
     {
+        startTime = Time.time;
+        deltaTime = startTime;
         boat = GameObject.Find("Boat");
+        initialPosition = boat.transform.position.z;
+        boat.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, initialVelocity);
     }
 
     // Update is called once per frame
     void Update()
     {
-         mass = 0;
-         centerOfMassZ = 0;
-         centerOfMassX = 0;
-         momentOfInertia = 0;
-        Vector3 force = new Vector3(Input.GetAxis("Horizontal") * 1000000, 0, Input.GetAxis("Vertical") * 1000000);
-        boat.GetComponent<Rigidbody>().AddForce(force);
+        mass = 0;
+        centerOfMassZ = 0;
+        centerOfMassX = 0;
+        momentOfInertia = 0;
+
+        //Vector3 force = new Vector3(Input.GetAxis("Horizontal") * 1000000, 0, Input.GetAxis("Vertical") * 1000000);
+        //boat.GetComponent<Rigidbody>().AddForce(force);
 
         Rigidbody[] bodies = boat.GetComponentsInChildren<Rigidbody>();
         foreach (Rigidbody body in bodies)
@@ -34,15 +57,15 @@ public class Move : MonoBehaviour
             string name = body.gameObject.name;
             if (name != "Boat")
             {
-                Debug.Log("Mass of " + name + ": " + body.mass);
-                Debug.Log("Position of " + name + ": " + body.gameObject.transform.localPosition);
+                //Debug.Log("Mass of " + name + ": " + body.mass);
+                // Debug.Log("Position of " + name + ": " + body.gameObject.transform.localPosition);
                 mass += body.mass;
             }
             centerOfMassX += body.gameObject.transform.localPosition.x * body.mass;
             centerOfMassZ += body.gameObject.transform.localPosition.z * body.mass;
         }
 
-       //Debug.Log("Mass of " + boat.name + ": " + mass);
+        //Debug.Log("Mass of " + boat.name + ": " + mass);
         boat.GetComponent<Rigidbody>().mass = mass;
         centerOfMassZ /= mass;
         centerOfMassX /= mass;
@@ -54,17 +77,70 @@ public class Move : MonoBehaviour
             if (name != "Boat")
             {
                 float h2 = Mathf.Pow(centerOfMassX - body.gameObject.transform.localPosition.x, 2) + Mathf.Pow(centerOfMassZ - body.gameObject.transform.localPosition.z, 2);
-                Debug.Log("h^2 for " + name + ": " + h2);
+                //Debug.Log("h^2 for " + name + ": " + h2);
                 float Mh2 = h2 * body.mass;
-                Debug.Log("Mh^2 for " + name + ": " + Mh2);
+                //Debug.Log("Mh^2 for " + name + ": " + Mh2);
                 float ly = body.mass * (Mathf.Pow(body.gameObject.transform.localScale.x, 2) + Mathf.Pow(body.gameObject.transform.localScale.z, 2)) / 12;
                 float inertia = ly + Mh2;
-                Debug.Log("Moment of Inertia for " + name + ": " + inertia);
+                //Debug.Log("Moment of Inertia for " + name + ": " + inertia);
 
                 momentOfInertia += inertia;
             }
         }
 
         //Debug.Log("moment of Inertia for " + boat.name + ": " + momentOfInertia);
+    }
+
+    private void FixedUpdate()
+    {
+        deltaTime += Time.deltaTime;
+
+
+        if (thing == 0)
+        {
+            if (boat.GetComponent<Rigidbody>().velocity.z > 0)
+            {
+                currentTime = deltaTime - startTime;
+                Debug.Log(boat.GetComponent<Rigidbody>().velocity);
+                Debug.Log(currentTime + " " + totalTime);
+
+                distance = initialPosition + initialVelocity * currentTime + 0.5 * initialAcceleration * Mathf.Pow(currentTime, 2);
+
+                boat.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, initialVelocity + initialAcceleration * currentTime);
+            }
+            if (currentTime >= totalTime)
+            {
+                boat.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+                stopPosition = distance;
+            }
+            if (boat.GetComponent<Rigidbody>().velocity.z < 0)
+            {
+                boat.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            }
+        }
+        else if (thing == 1)
+        {
+            if (boat.GetComponent<Rigidbody>().velocity.z <= 0 || boat.transform.position.z >=16)
+            {
+                //boat.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+                totalTime = currentTime;
+                Debug.Log(totalTime);
+            }
+            else
+            {
+                currentTime = deltaTime - startTime;
+            }
+            //Debug.Log(distance + " " + stopPosition);
+            dragConstant = -initialAcceleration / Mathf.Pow(initialVelocity, 2);
+            //Debug.Log(dragConstant);
+            //Debug.Log(boat.GetComponent<Rigidbody>().velocity.z);
+            //Debug.Log(Mathf.Pow(boat.GetComponent<Rigidbody>().velocity.z, 2));
+            //Debug.Log(dragConstant);
+            distance = initialPosition + (Mathf.Log(1 + initialVelocity * currentTime, (float)Math.E) / dragConstant);
+            //Debug.Log(initialVelocity / (1 + dragConstant * initialVelocity * currentTime));
+            boat.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, (float)(initialVelocity / (1 + dragConstant * initialVelocity * currentTime)));
+
+        }
+        actualBoatPosition = boat.transform.position.z;
     }
 }
