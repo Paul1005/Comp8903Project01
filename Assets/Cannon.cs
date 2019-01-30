9abb2e7bf45e3006bfb77ea5caf85d6a02b46b31
Paul1005 <mccarliep@gmail.com>
2019-01-29 23:56:36 -0800

    started project 3

---
+++ Assets/Cannon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+
+    public float initialVelocity;
+    private float gravity;
+    public float range;
+    public float angle;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject target = GameObject.Find("Target");
+        gravity = 9.81f;
+        range = target.transform.position.z - gameObject.transform.position.z;
+        GameObject gunball = GameObject.Find("Gunball");
+
+        //do calculations here
+        angle = Mathf.Asin(gravity * range / (Mathf.Pow(initialVelocity, 2) * 2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
