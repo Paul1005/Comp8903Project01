db9932561ea638aa0ba748bb37f2c8a727bb9118
Paul1005 <mccarliep@gmail.com>
2019-03-01 21:03:37 -0800

    getting there

--- Assets/AngularMove.cs
+++ Assets/AngularMove.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AngularMove : MonoBehaviour {
+public class AngularMove : MonoBehaviour
+{
     public float force;
     public Vector3 dFinalLeft;
     public Vector3 dFinalRight;
     public int hullMass;
     public int gunMass;
     public int pilotMass;
+    public float momentOfInertia;
 
-    private Vector3 tFinalLeft;
-    private Vector3 tFinalRight;
+    public Vector3 tFinalLeft;
+    public Vector3 tFinalRight;
     private Vector3 rLeft;
     private Vector3 rRight;
     private Vector3 torqueLeft;
     private Vector3 torqueRight;
-    private Vector3 accLeft;
-    private Vector3 accRight;
+    public Vector3 accLeft;
+    public Vector3 accRight;
     private Vector3 thrust;
     private Vector3 comPosition;
     private GameObject hull;
@@ -25,19 +27,47 @@ public class AngularMove : MonoBehaviour {
     private GameObject pilot;
     private int comMass;
     private int ticks;
+    public float angle;
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         ticks = 0;
+
         comMass = hullMass + gunMass + pilotMass;
         hull = GameObject.Find("Hull");
         gun = GameObject.Find("Gun");
         pilot = GameObject.Find("Pilot");
+
         comPosition.x = (hull.transform.position.x * hullMass + gun.transform.position.x * gunMass + pilot.transform.position.x * pilotMass) / comMass;
         comPosition.z = (hull.transform.position.z * hullMass + gun.transform.position.z * gunMass + pilot.transform.position.z * pilotMass) / comMass;
+
+        float lyHull = hullMass * (Mathf.Pow(hull.transform.localScale.x, 2) * Mathf.Pow(hull.transform.localScale.z, 2)) / 12;
+        float h2Hull = Mathf.Pow(comPosition.x-hull.transform.position.x, 2) + Mathf.Pow(comPosition.z - hull.transform.position.z, 2);
+        float mh2Hull = h2Hull * hullMass;
+        float inertiaHull = lyHull + mh2Hull;
+
+        float lyPilot = hullMass * (Mathf.Pow(pilot.transform.localScale.x, 2) * Mathf.Pow(pilot.transform.localScale.z, 2)) / 12;
+        float h2Pilot = Mathf.Pow(comPosition.x - pilot.transform.position.x, 2) + Mathf.Pow(comPosition.z - pilot.transform.position.z, 2);
+        float mh2Pilot = h2Pilot * pilotMass;
+        float inertiaPilot = lyPilot + mh2Pilot;
+
+        momentOfInertia = inertiaHull + inertiaPilot;
+
+        thrust = new Vector3(force * Mathf.Sin(angle * Mathf.Deg2Rad), 0, force * Mathf.Cos(angle * Mathf.Deg2Rad));
+
+        rLeft = new Vector3(2 - comPosition.x, 0, 4 - comPosition.z);
+        rRight = new Vector3(-2 - comPosition.x, 0, -4 - comPosition.z);
+
+        torqueLeft.y = rLeft.z * thrust.x - rLeft.x * thrust.z;
+        torqueRight.y = rRight.z * thrust.x - rRight.x * thrust.z;
+
+        gameObject.transform.eulerAngles = new Vector3(0, angle, 0);
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		
-	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
 }
