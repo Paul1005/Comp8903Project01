0e1d233050a8ea6efe613c9aadb593b5e8672032
Paul1005 <mccarliep@gmail.com>
2019-03-20 23:51:31 -0700

    some more setup

--- Assets/CollisionManager.cs
+++ Assets/CollisionManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CollisionManager : MonoBehaviour
 {
-
     public float ballMass;
     public float initialBallVelocity;
     public float finalBallVelocity;
@@ -19,22 +18,35 @@ public class CollisionManager : MonoBehaviour
     public float j;
     public float initialTotalMomentum;
     public float finalTotalMomentum;
+    public float vr;
+
+    private Rigidbody gunBall;
+    private Rigidbody target;
 
     // Use this for initialization
     void Start()
     {
-        float vr = initialBallVelocity - initialTargetVelocity;
+        vr = initialBallVelocity - initialTargetVelocity;
         j = -vr * (e + 1) * (ballMass * targetMass) / (ballMass + targetMass);
-        finalBallVelocity = j/ballMass + initialBallVelocity;
-        finalTargetVelocity = -j/targetMass + initialTargetVelocity;
+        finalBallVelocity = j / ballMass + initialBallVelocity;
+        finalTargetVelocity = -j / targetMass + initialTargetVelocity;
 
         initialBallMomentum = initialBallVelocity * ballMass;
-        initialTargetMomentum = initialTargetMomentum * targetMass;
+        initialTargetMomentum = initialTargetVelocity * targetMass;
         finalBallMomentum = finalBallVelocity * ballMass;
         finalTargetMomentum = finalTargetVelocity * targetMass;
 
         initialTotalMomentum = initialTargetMomentum + initialBallMomentum;
         finalTotalMomentum = finalTargetMomentum + finalBallMomentum;
+
+        gunBall = GameObject.Find("Gunball").GetComponent<Rigidbody>();
+        target = GameObject.Find("Target").GetComponent<Rigidbody>();
+
+        gunBall.velocity = new Vector3(0, 0, initialBallVelocity);
+        target.velocity = new Vector3(0, 0, -initialTargetVelocity);
+
+        gunBall.mass = ballMass;
+        target.mass = targetMass;
     }
 
     // Update is called once per frame
