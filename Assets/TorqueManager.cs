2c8d28eb406b586603f460b11aa6beb774fbc024
Paul1005 <mccarliep@gmail.com>
2019-04-08 22:39:34 -0700

    started project 11

---
+++ Assets/TorqueManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorqueManager : MonoBehaviour {
+    public float gunBallMass;
+    public float targetMass;
+    public float initialGunBallVelocity;
+    public float initialTargetVelocity;
+    public float finalGunBallVelocity;
+    public float finalTargetVelocity;
+    public float e;
+    public float jn;
+    public Vector3 P;
+    public Vector3 R1;
+    public Vector3 R2;
+    public float initialGunBallMomentum;
+    public float initialTargetMomentum;
+    public float initialTotalMomentum;
+    public float finalGunBallMomentum;
+    public float finalTargetMomentum;
+    public float finalTotalMomentum;
+    public float initialGunBallKE;
+    public float initialTargetKE;
+    public float initialTotalKE;
+    public Vector3 r1;
+    public Vector3 r2;
+    public float initialGunBallAngularMomentum;
+    public float initialTargetAngularMomentum;
+    public float initialTotalAngularMomentum;
+    public float finalGunBallAngularMomentum;
+    public float finalTargetAngularMomentum;
+    public float finalTotalAngularMomentum;
+    public float finalGunBallKE;
+    public float finalTargetKE;
+    public float finalTotalKE;
+    public float finalGunBallRKE;
+    public float finalTargetRKE;
+    public float finalTotalRKE;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+}
